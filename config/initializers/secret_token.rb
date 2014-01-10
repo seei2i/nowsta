1c108b7678e6e2fa27a1124d8b17d2b78087611f
Justin Lynch <justinmatthewlynch@gmail.com>
2014-01-10 13:15:26 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nowsta::Application.config.secret_key_base = '9bed6625907afc19f451abe932742e336aeb144e947287cad28bd6b072d4425282b3ce28c757357e5be4d0241a5841a3cfa170fe6a3660a63dba7491c4be0c0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nowsta::Application.config.session_store :cookie_store, key: '_nowsta_session'
