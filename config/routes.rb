e656b80f3a3833ff3eb7a762e5c0549ef12341a3
Justin Lynch <justinmatthewlynch@gmail.com>
2014-01-27 14:24:20 -0500

    fejief

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Nowsta::Application.routes.draw do
   resources :people
   resources :events
+  devise_for :users, :controllers => {:registrations => "registrations", :users => "users"}
   
+  root :to => "home#index"
+  resources :users
 
   match '/:id' => "shortener/shortened_urls#show", via: [:get, :post]
 
-  
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
-  root :to => "home#index"
 end
