0316b72aed012307e8950bd577224a5af2440863
Justin Lynch <justinmatthewlynch@gmail.com>
2014-02-12 15:15:24 -0500

    etff

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module Nowsta
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config.assets.enabled = true
-    # config.assets.initialize_on_precompile = false 
+    config.assets.initialize_on_precompile = false 
   end
 end
